b4405dd3e0b5b4d24cb5302e3979811e082736f8
Ilya Zverev <zzvial@gmail.com>
2021-01-11 13:43:33 +0700

    Homework #7
    Networking
    
    Добавлена загрузка данных из remote API для экрана списка и экрана детальных данных

--- app/src/main/java/com/example/superpupermegaproject/model/RemoteAPIInteractor.kt
+++ app/src/main/java/com/example/superpupermegaproject/model/RemoteAPIInteractor.kt
@@ -3,10 +3,7 @@ package com.example.superpupermegaproject.model
 import android.util.Log
 import com.example.superpupermegaproject.BuildConfig
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
@@ -19,18 +16,18 @@ class RemoteAPIInteractor {
         ignoreUnknownKeys = true
     }
 
-    private val remoteRequestsCoroutineContext = Dispatchers.IO
-    private lateinit var retrofitInstance: Retrofit
+    private val handlerException = CoroutineExceptionHandler{ context, throwable ->
+        Log.d(this.javaClass.canonicalName, throwable.localizedMessage)
+        throwable.printStackTrace()
+    }
+    private val remoteRequestsCoroutineContext = Dispatchers.IO + SupervisorJob() + handlerException
     private lateinit var remoteApi: TmDBAPI
-    private lateinit var configurationResponse: ConfigurationResponse
     private var imageURL: String = ""
     private var backdropSize: String = "original"
     private var posterSize: String = "original"
 
     init{
         initRetrofit()
-
-        remoteApi = retrofitInstance.create(TmDBAPI::class.java)
 
         CoroutineScope(remoteRequestsCoroutineContext).launch {
             loadConfig()
@@ -41,11 +38,7 @@ class RemoteAPIInteractor {
         val moviesList = mutableListOf<MovieItemResponse>()
         var response: MoviesListResponse? = null
         withContext(remoteRequestsCoroutineContext) {
-            try {
-                response = remoteApi.getMovies()
-            } catch (e: Exception) {
-                Log.d(this.javaClass.canonicalName, e.localizedMessage)
-            }
+            response = remoteApi.getMovies()
         }
 
         response?.let { movieResponse ->
@@ -85,11 +78,7 @@ class RemoteAPIInteractor {
         val genresList = mutableListOf<GenreResponse>()
         var response: GenresResponse? = null
         withContext(remoteRequestsCoroutineContext) {
-            try {
-                response = remoteApi.getGenres()
-            } catch (e: Exception) {
-                Log.d(this.javaClass.canonicalName, e.localizedMessage)
-            }
+            response = remoteApi.getGenres()
         }
 
         response?.let { genresResponse ->
@@ -108,19 +97,17 @@ class RemoteAPIInteractor {
             .addInterceptor(loggingInterceptor)
             .build()
 
-        retrofitInstance = Retrofit.Builder()
+        val retrofitInstance = Retrofit.Builder()
             .baseUrl(BuildConfig.API_BASE_URL)
             .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
             .client(okHttpClient)
             .build()
+
+        remoteApi = retrofitInstance.create(TmDBAPI::class.java)
     }
 
     private suspend fun loadConfig() {
-        try {
-            configurationResponse = remoteApi.getConfiguration()
-        } catch (e: Exception) {
-            Log.d(this.javaClass.canonicalName, e.localizedMessage)
-        }
+        val configurationResponse = remoteApi.getConfiguration()
 
         imageURL = configurationResponse.images.secureBaseURL
         backdropSize = configurationResponse.images.backdropSizes[1]
