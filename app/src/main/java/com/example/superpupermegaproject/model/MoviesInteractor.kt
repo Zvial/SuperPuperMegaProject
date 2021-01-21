58e04518bf3a77c0c3eb6d579ba21e64012d4ede
Ilya Zverev <zzvial@gmail.com>
2021-01-21 10:15:29 +0700

    Homework #7
    Networking
    
    Добавлен класс Mapper для преобразования данных, поступающих от сервера в бизнес-данные

--- app/src/main/java/com/example/superpupermegaproject/model/MoviesInteractor.kt
+++ app/src/main/java/com/example/superpupermegaproject/model/MoviesInteractor.kt
@@ -3,16 +3,19 @@ package com.example.superpupermegaproject.model
 import com.example.superpupermegaproject.data.Actor
 import com.example.superpupermegaproject.data.Genre
 import com.example.superpupermegaproject.data.Movie
+import com.example.superpupermegaproject.model.api_responses.GenreResponse
+import com.example.superpupermegaproject.model.api_responses.MovieDetailResponse
 import com.example.superpupermegaproject.model.api_responses.MovieItemResponse
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MoviesInteractor private constructor(
-    private val repository: Repository
+        private val repository: Repository
 ) {
     private val genresCache: MutableList<Genre> = mutableListOf()
     private val runtimesCache = mutableMapOf<Int, Int>()
+    private val mapper = Mapper()
 
     init {
         CoroutineScope(Dispatchers.IO).launch {
@@ -21,23 +24,23 @@ class MoviesInteractor private constructor(
     }
 
     suspend fun searchMovies(
-        query: String,
-        isNewQuery: Boolean,
-        loadRuntimeValue: Boolean = false
+            query: String,
+            isNewQuery: Boolean,
+            loadRuntimeValue: Boolean = false
     ): List<Movie> {
         if (isNewQuery) repository.clearLastPage()
         return repository.searchMovies(query).map { movieResponse ->
-            createMovie(movieResponse, loadRuntimeValue)
+            mapper.createMovieInstance(movieResponse, loadRuntimeValue)
         }
     }
 
     suspend fun getMovies(
-        isNewQuery: Boolean,
-        loadRuntimeValue: Boolean = false
+            isNewQuery: Boolean,
+            loadRuntimeValue: Boolean = false
     ): List<Movie> {
         if (isNewQuery) repository.clearLastPage()
         return repository.loadMovies().map { movieResponse ->
-            createMovie(movieResponse, loadRuntimeValue)
+            mapper.createMovieInstance(movieResponse, loadRuntimeValue)
         }
     }
 
@@ -46,68 +49,74 @@ class MoviesInteractor private constructor(
         if (movieResponse == null)
             return null
 
-        val genres = movieResponse.genres.map { genreResponse ->
-            Genre(genreResponse.id.toInt(), genreResponse.name)
-        }
-        return Movie(
-            movieResponse.id,
-            movieResponse.title,
-            movieResponse.overview,
-            poster = movieResponse.posterPath ?: "",
-            backdrop = movieResponse.backdropPath,
-            movieResponse.voteAverage.toFloat(),
-            movieResponse.adult,
-            movieResponse.runtime.toInt(),
-            genres,
-            getMovieActors(movieResponse.id.toInt()),
-            movieResponse.voteCount.toInt(),
-            false
-        )
+        return mapper.createMovieInstance(movieResponse, true)
     }
 
-    private suspend fun createMovie(
-        movieResponse: MovieItemResponse,
-        loadRuntimeValue: Boolean
-    ): Movie =
-        Movie(
-            movieResponse.id,
-            movieResponse.title,
-            movieResponse.overview,
-            poster = movieResponse.posterPath,
-            backdrop = movieResponse.backdropPath,
-            movieResponse.voteAverage.toFloat(),
-            movieResponse.adult,
-            if (loadRuntimeValue) getRuntimeValueForMovie(movieResponse.id.toInt()) else 0,
-            genresCache.filter { genre ->
-                movieResponse.genreIDS.contains(genre.id.toLong())
-            },
-            listOf(),
-            movieResponse.voteCount.toInt(),
-            false
-        )
-
     private suspend fun getRuntimeValueForMovie(movieID: Int) =
-        if (runtimesCache.containsKey(movieID)) {
-            runtimesCache[movieID]!!
-        } else {
-            val value = repository.loadMovie(movieID)?.runtime?.toInt() ?: 0
-            runtimesCache.put(movieID, value)
-            value
-        }
+            if (runtimesCache.containsKey(movieID)) {
+                runtimesCache[movieID]!!
+            } else {
+                val value = repository.loadMovie(movieID)?.runtime?.toInt() ?: 0
+                runtimesCache.put(movieID, value)
+                value
+            }
 
     private suspend fun getMovieActors(movieID: Int) = repository.loadMovieActors(movieID)
-        .map { castResponse ->
-            Actor(
-                id = castResponse.id.toInt(),
-                name = castResponse.name,
-                picture = castResponse.profilePath
-            )
-        }
+            .map { castResponse ->
+                Actor(
+                        id = castResponse.id.toInt(),
+                        name = castResponse.name,
+                        picture = castResponse.profilePath
+                )
+            }
 
     private suspend fun loadGenres() {
         genresCache.addAll(repository.loadGenres().map {
-            Genre(it.id.toInt(), it.name)
+            mapper.createGenreInstance(it)
         })
+    }
+
+    inner class Mapper {
+        suspend fun createMovieInstance(from: MovieItemResponse, loadRuntimeValue: Boolean): Movie =
+                Movie(
+                        from.id,
+                        from.title,
+                        from.overview,
+                        poster = from.posterPath ?: "",
+                        backdrop = from.backdropPath ?: "",
+                        from.voteAverage.toFloat(),
+                        from.adult,
+                        if (loadRuntimeValue) getRuntimeValueForMovie(from.id.toInt()) else 0,
+                        genresCache.filter { genre ->
+                            from.genreIDS.contains(genre.id.toLong())
+                        },
+                        listOf(),
+                        from.voteCount.toInt(),
+                        false
+                )
+
+        suspend fun createMovieInstance(from: MovieDetailResponse, loadRuntimeValue: Boolean): Movie {
+            val genres = from.genres.map { genreResponse ->
+                createGenreInstance(genreResponse)
+            }
+
+            return Movie(
+                    from.id,
+                    from.title,
+                    from.overview,
+                    poster = from.posterPath ?: "",
+                    backdrop = from.backdropPath ?: "",
+                    from.voteAverage.toFloat(),
+                    from.adult,
+                    from.runtime.toInt(),
+                    genres,
+                    getMovieActors(from.id.toInt()),
+                    from.voteCount.toInt(),
+                    false
+            )
+        }
+
+        suspend fun createGenreInstance(from: GenreResponse): Genre = Genre(from.id.toInt(), from.name)
     }
 
     companion object {
